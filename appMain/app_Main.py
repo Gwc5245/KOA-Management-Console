ac498970239cc354af9180f806686a13a76ff08e
tvg5197 <51892545+tvg5197@users.noreply.github.com>
2022-10-15 16:29:24 -0400

    Initial commit - directories

--- appMain/app_Main.py
+++ appMain/app_Main.py
@@ -1,6 +1,13 @@
 import tkinter as tk
 
-window = tk.Tk()
-label = tk.Label(text="KOA Management Console", fg="white", bg="black")
-label.pack()
-window.mainloop()
+
+class appWindowMain:
+    def openLoginScreen(self):
+        window = tk.Tk()
+        label = tk.Label(text="KOA Management Console", fg="white", bg="black")
+        label.pack()
+        window.mainloop()
+
+
+windowMain = appWindowMain()
+windowMain.openLoginScreen()
