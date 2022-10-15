4351686404083046a00b57ed98f4580f378cd0ef
tvg5197 <51892545+tvg5197@users.noreply.github.com>
2022-10-15 16:41:34 -0400

    converted TK to PySimpleGUI

--- appMain/app_Main.py
+++ appMain/app_Main.py
@@ -1,12 +1,29 @@
 import tkinter as tk
+import PySimpleGUI as sg
 
 
 class appWindowMain:
     def openLoginScreen(self):
-        window = tk.Tk()
-        label = tk.Label(text="KOA Management Console", fg="white", bg="black")
-        label.pack()
-        window.mainloop()
+        layout2 = [
+            [sg.Text("Please login to continue.")],
+            [sg.Text('Username', size=(15, 1)), sg.InputText('')],
+            [sg.Text('Password', size=(15, 1)), sg.InputText('', key='Password', password_char='*')],
+            [sg.Button("OK")]
+        ]
+        window = sg.Window(title="KOA Management Console Login", layout=layout2, margins=(500, 500)).read()
+
+        # label = tk.Label(text="KOA Management Console", fg="white", bg="black")
+
+        # label.pack()
+        # window.mainloop()
+        while True:
+            event, values = window.read()
+            # End program if user closes window or
+            # presses the OK button
+            if event == "OK" or event == sg.WIN_CLOSED:
+                break
+
+        window.close()
 
 
 windowMain = appWindowMain()
