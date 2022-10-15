15eec9761a59aee5a0c1fd4b6630e3876dc89ddf
tvg5197 <51892545+tvg5197@users.noreply.github.com>
2022-10-15 17:36:37 -0400

    added all elements .py for simplegui

--- appMain/app_Main.py
+++ appMain/app_Main.py
@@ -3,15 +3,17 @@ import PySimpleGUI as sg
 
 
 class appWindowMain:
+    _userName = ''
+
     def openLoginScreen(self):
-        layout2 = [
+        layout = [
             [sg.Text("Please login to continue.")],
             [sg.Text('Username', size=(15, 1)), sg.InputText('', key='Username')],
             [sg.Text('Password', size=(15, 1)), sg.InputText('', key='Password', password_char='*')],
             [sg.Button("OK")]
         ]
         # window = sg.Window(title="KOA Management Console Login", layout=layout2, margins=(500, 500)).read()
-        window = sg.Window(title="KOA Management Console Login", layout=layout2)
+        window = sg.Window(title="KOA Management Console Login", layout=layout)
 
         # label = tk.Label(text="KOA Management Console", fg="white", bg="black")
         # label.pack()
@@ -21,11 +23,19 @@ class appWindowMain:
             # End program if user closes window or
             # presses the OK button
             if event == "OK" or event == sg.WIN_CLOSED:
+                _userName = values['Username']
                 break
 
         window.close()
-        print('Username: ' + values['Username'])
+        # print('Username: ' + values['Username'])
+        print('Username: ' + _userName)
         print('Password: ' + values['Password'])
+
+    def openWelcomeScreen(self):
+        layout = [
+            [sg.Text("KOA Management Console: Welcome.")],
+        ]
+        window = sg.Window(title="KOA Management Console", layout=layout)
 
 
 windowMain = appWindowMain()
