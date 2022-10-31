d7162ff3d4f5bb4541e66ca03bcc1f7a972631e8
tvg5197 <51892545+tvg5197@users.noreply.github.com>
2022-10-31 19:33:14 -0400

    added ability to add stations to mongodb

--- appMain/app_Main.py
+++ appMain/app_Main.py
@@ -2,7 +2,6 @@ import hashlib
 import PySimpleGUI as sg
 import os
 
-import aws as aws
 import bcrypt
 
 import pymongo as pymongo
@@ -11,17 +10,14 @@ from pymongo.server_api import ServerApi
 path = os.path.abspath(__file__)
 sg.theme("reddit")
 
-client = pymongo.MongoClient("mongodb+srv://<AWS access key>:<AWS secret key>@cluster0"
-                             ".re3ie7p.mongodb.net/?authSource=%24external&authMechanism=MONGODB-AWS&retryWrites=true"
-                             "&w=majority", server_api=ServerApi('1'))
-
+client = pymongo.MongoClient("mongodb+srv://<AWS access key>:<AWS secret key>@cluster0.re3ie7p.mongodb.net/?authSource=%24external&authMechanism=MONGODB-AWS&retryWrites=true&w=majority", server_api=ServerApi('1'))
 
 db = client.KOADB
 
-print(db.list_collection_names())
+print("Collections: ", db.list_collection_names())
 print(client.server_info())
 # testing inserting collections into mongodb.
-print(db.test_collection.insert_one({"my_test field": "my test value oct 24 2022"}))
+print(db.test_collection.insert_one({"my_test field": "my test value oct 30 2022"}))
 
 
 # Calculates the hash of the directory the python file is in.
@@ -72,6 +68,42 @@ class appWindowMain:
         print('Username: ' + self._userName)
         print('Password: ' + values['Password'])
 
+    def openAddStation(self):
+        layout = [
+            [sg.Text("Enter weather station info to add it to the database.")],
+            [sg.Text('Station Name', size=(15, 1)), sg.InputText('', key='Station Name')],
+            [sg.Text('Station Street', size=(15, 1)), sg.InputText('', key='Station Street')],
+            [sg.Text('Station Municipality', size=(15, 1)), sg.InputText('', key='Station Municipality')],
+            [sg.Text('Station State', size=(15, 1)), sg.InputText('', key='Station State')],
+            [sg.Text('Station Zip Code', size=(15, 1)), sg.InputText('', key='Station Zip Code')],
+            [sg.Button("OK")],
+            [sg.Button("Cancel")],
+
+        ]
+        # window = sg.Window(title="KOA Management Console Login", layout=layout2, margins=(500, 500)).read()
+        window = sg.Window(title="KOA Management Console Add Station", layout=layout)
+
+        # label = tk.Label(text="KOA Management Console", fg="white", bg="black")
+        # label.pack()
+        # window.mainloop()
+        while True:
+            event, values = window.read()
+            # End program if user closes window or
+            # presses the OK button
+            if event == "OK":
+                # df = [values["Station Name"],values["Station Street"], values["Station Municipality"], values["Station State"], values["Station Zip Code"]]
+                # convert_dict = df.to_dict(orient="records")
+                weatherDict = {"name": values["Station Name"], "street": values["Station Street"],
+                               "municipality": values["Station "
+                                                      "Municipality"],
+                               "state": us_state_to_abbrev[values["Station State"]],
+                               "zip code": values["Station Zip Code"]}
+                db.WeatherStations.insert_one(weatherDict)
+                break
+            if event == "Cancel":
+                window.close()
+        window.close()
+
     # Opens the main dashboard that the user first sees after login, presenting to them a menu of options.
     def openWelcomeScreen(self, stations):
         username = (self.getCurrentUser())
@@ -95,6 +127,7 @@ class appWindowMain:
                 break
             elif values["-ADD-"]:
                 print("Add is selected.")
+                self.openAddStation()
 
     # Returns all the weather stations.
     def getSensors(self):
@@ -129,6 +162,67 @@ class appWindowMain:
         # print("Retrieved salt from file. Salt: ", listSaltByte)
         return salt
 
+
+# Source: https://gist.github.com/rogerallen/1583593
+us_state_to_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "U.S. Virgin Islands": "VI",
+}
 
 windowMain = appWindowMain()
 windowMain.genSalt()
